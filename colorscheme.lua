a73fba034d6c478d0961763f2ee89276f5d1ea0d
Martin Vysny <martin@vysny.me>
2026-06-03 14:56:06 +0300

    use catppuccin-latte as a theme

---
+++ colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  -- Set the colorscheme LazyVim loads at startup
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",  -- replace with your theme
+    },
+  },
+}
